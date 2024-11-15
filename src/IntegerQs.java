b01a3e17f2208edb94e584abd4f07b2e7a51fb12
Faique Inaas MZM <faiqueinaas@gmail.com>
2024-11-14 16:43:08 -0800

    added new methods

--- src/IntegerQs.java
+++ src/IntegerQs.java
@@ -20,6 +20,25 @@ public class IntegerQs {
         return largest;
     }
 
+    public static int sumOfDigits(int n) {
+        int sum = 0;
+        while (n != 0) {
+            sum += n % 10;
+            n /= 10;
+        }
+        return sum;
+    }
+
+    public static int reverseInteger(int n) {
+        int reverse = 0;
+        while (n != 0) {
+            int digit = n % 10;
+            reverse = reverse * 10 + digit;
+            n /= 10;
+        }
+        return reverse;
+    }
+
     public static void main(String[] args) {
 
         int[] numArr = {7,2,8,4,6,2,1};
@@ -27,5 +46,13 @@ public class IntegerQs {
         System.out.println("Array : " + Arrays.toString(numArr));
         System.out.println("Largest number in the array is " + findLargestNumber1(numArr));
         System.out.println("Largest number in the array is " + findLargestNumber2(numArr));
+        System.out.println();
+
+        int num = 1234567;
+        System.out.println("Sum of the digits of " + num + " is " + sumOfDigits(num));
+        System.out.println();
+
+        System.out.println("Reversed number of " + num + " is " + reverseInteger(num));
+        System.out.println();
     }
 }
